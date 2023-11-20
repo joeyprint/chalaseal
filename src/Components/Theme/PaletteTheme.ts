94b0c10c791b20d6401f1a9af4b0e859e2b08730
Joey Wasin <wasin.ww1402@gmail.com>
2023-11-20 12:45:04 +0700

    :lipstick: Create palette theme

---
+++ src/Components/Theme/PaletteTheme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material';
+
+const paletteTheme = createTheme({
+  palette: {
+    primary: {
+      light: 'hsla(266, 41%, 40%, 1)',
+      main: 'hsla(266, 41%, 28%, 1)',
+      dark: 'hsla(266, 41%, 15%, 1)',
+    },
+  },
+});
+
+export default paletteTheme;
