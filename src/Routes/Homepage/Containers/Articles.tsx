1a155393f6e34dce9773f7a9755b8f693971c85f
Joey Wasin <wasin.ww1402@gmail.com>
2023-11-20 16:21:44 +0700

    :clown_face: Mockup ui of article item

--- src/Routes/Homepage/Containers/Articles.tsx
+++ src/Routes/Homepage/Containers/Articles.tsx
@@ -1,14 +1,33 @@
 import { Box, Typography } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+
+import ArticleItem from '../Components/ArticleItem';
 
 const Articles = () => {
+  const navigate = useNavigate();
+
+  const goToArticleById = (id: string) => {
+    navigate(`/articles/${id}`);
+  };
+
   return (
-    <Box display={'flex'} justifyContent={'space-between'} mt={8}>
-      <Typography variant={'h5'}>Items</Typography>
-      <Typography component={Link} to={'/browse'}>
-        See More
-      </Typography>
-    </Box>
+    <>
+      <Box display={'flex'} justifyContent={'space-between'} mt={8}>
+        <Typography variant={'h5'}>Items</Typography>
+        <Typography component={Link} to={'/browse'}>
+          See More
+        </Typography>
+      </Box>
+      <Box>
+        <ArticleItem
+          topic={'Yao: Ban Huai Mae Sai Dialect'}
+          description={
+            'Interviews of Yao people, recording their rituals and tone in Yao, and a list of approximately 700 words, which recorded with pencil in Thai and word prompts are written in Thai'
+          }
+          onClick={() => goToArticleById('1')}
+        />
+      </Box>
+    </>
   );
 };
 
