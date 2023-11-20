edfce9eca27bbd5ac8e7b9a6151d5bbc57cef17e
Joey Wasin <wasin.ww1402@gmail.com>
2023-11-20 13:34:19 +0700

    :lipstick: Create appbar with chula logo

--- src/Components/Layout/BaseLayout.tsx
+++ src/Components/Layout/BaseLayout.tsx
@@ -1,14 +1,28 @@
 import { AppBar, Box, Container, Toolbar, Typography } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 
+import { Image } from '../Base';
+
 const BaseLayout = () => {
   return (
     <Box>
       <AppBar>
-        <Container sx={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Typography>Logo</Typography>
-          <Box>
-            <Typography>Articles</Typography>
+        <Container
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+        >
+          <Image
+            src={'/assets/images/chula-seal-logo.svg'}
+            alt={'Chula SEAL Logo'}
+            maxHeight={48}
+          />
+          <Box display={'flex'}>
+            <Typography>Browse</Typography>
+            <Typography pl={2}>About</Typography>
+            <Typography pl={2}>Contact Us</Typography>
           </Box>
         </Container>
       </AppBar>
