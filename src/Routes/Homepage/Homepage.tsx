ed355440e44f035cffdf40a6246decc28cb45a67
Joey Wasin <wasin.ww1402@gmail.com>
2023-11-22 09:11:06 +0700

    :lipstick: Integrate authors container into page

--- src/Routes/Homepage/Homepage.tsx
+++ src/Routes/Homepage/Homepage.tsx
@@ -1,11 +1,13 @@
 import { Container } from '@mui/material';
 
 import Articles from './Containers/Articles';
+import Authors from './Containers/Authors';
 
 const Homepage = () => {
   return (
     <Container>
       <Articles />
+      <Authors />
     </Container>
   );
 };
