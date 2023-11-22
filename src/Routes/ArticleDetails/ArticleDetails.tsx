89d0d739071ff69591148a458f31ae7d741f47d7
Joey Wasin <wasin.ww1402@gmail.com>
2023-11-22 11:24:23 +0700

    :lipstick: Update navigation back

--- src/Routes/ArticleDetails/ArticleDetails.tsx
+++ src/Routes/ArticleDetails/ArticleDetails.tsx
@@ -1,15 +1,19 @@
 import WestRoundedIcon from '@mui/icons-material/WestRounded';
 import { Button, Container } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 import ArticleBasicInformation from './Containers/ArticleBasicInformation';
 
 const ArticleDetails = () => {
+  const navigate = useNavigate();
+
   return (
     <Container>
       <Button
         startIcon={<WestRoundedIcon />}
         size={'large'}
         variant={'outlined'}
+        onClick={() => navigate(-1)}
       >
         Back
       </Button>
