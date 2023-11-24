8f328e7caa9db22db702f1dbd0bdad914ee1846f
Joey Wasin <wasin.ww1402@gmail.com>
2023-11-24 11:14:35 +0700

    :lipstick: Add acknowledge information into about page

--- src/Routes/About/About.tsx
+++ src/Routes/About/About.tsx
@@ -1,11 +1,17 @@
 import { Container, Typography } from '@mui/material';
+
 import AboutInfo from './Containers/AboutInfo';
+import AcknowledgeInfo from './Containers/AcknowledgeInfo';
 
 const About = () => {
   return (
     <Container>
       <Typography variant={'h4'}>About</Typography>
       <AboutInfo />
+      <Typography variant={'h4'} mt={6}>
+        Acknowledgements
+      </Typography>
+      <AcknowledgeInfo />
     </Container>
   );
 };
---
+++ src/Routes/About/Containers/AcknowledgeInfo.tsx
@@ -0,0 +1,19 @@
+import { Box, Typography } from '@mui/material';
+
+const AcknowledgeInfo = () => {
+  return (
+    <Box mt={2}>
+      <Typography>
+        Dr. Pittayawat Pittayaporn played a pivotal role as both the initiator
+        and designer of this archive. The meticulous initial metadata design was
+        executed by Dr. Jinwat Kaenmuang and Dr. Warunsiri Pornphotthanamas,
+        while Suphitchaya Singsahuth contributed her expertise to curate and
+        construct this digital archive. The team also sought guidance from Dr.
+        Wachiraporn Klungthanaboon, who possesses expertise in digital archive
+        organization.
+      </Typography>
+    </Box>
+  );
+};
+
+export default AcknowledgeInfo;
