371d87de56fe941375b578136cf7edc11f484997
Joey Wasin <wasin.ww1402@gmail.com>
2023-12-14 16:11:18 +0700

    feat: wip try to push into staging

--- src/Routes/ArticleDetails/Containers/PdfViewer.tsx
+++ src/Routes/ArticleDetails/Containers/PdfViewer.tsx
@@ -1,12 +1,31 @@
 import { Alert, Box } from '@mui/material';
+import { useParams } from 'react-router-dom';
 import { Viewer } from '@react-pdf-viewer/core';
+
 import '@react-pdf-viewer/core/lib/styles/index.css';
 
+import { useItemDetails } from '../../../Domains/item';
+
 const PdfViewer = () => {
+  const { articleId = '' } = useParams();
+  const { data, isLoading, isError } = useItemDetails(articleId);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error fetching data</div>;
+  }
+
+  const articleFile = data?.data?.files;
+
+  if (!articleFile || articleFile.length === 0) return null;
+
   return (
     <Box mt={4}>
       <Viewer
-        fileUrl='https://www.chulaseal.com/field/files/original/7c88c9edd786d567591f33fe5c023b71.pdf'
+        fileUrl={articleFile[0].url}
         renderError={() => (
           <Alert severity={'error'}>Fail to fetch PDF file</Alert>
         )}
